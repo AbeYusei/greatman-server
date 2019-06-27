1adb18db704d9e775bf8cd8a59107cc048255743
阿部 悠世 <abe@goga.co.jp>
2019-06-28 01:37:13 +0900

    avoid cors (temp/dev uses)

--- src/api/main.go
+++ src/api/main.go
@@ -17,6 +17,7 @@ type arg struct {
 
 func main() {
 	router := gin.Default()
+	router.Use(cors())
 
 	m := msgAPIArg()
 	router.GET(m.path, m.handler)
@@ -50,4 +51,21 @@ func random(min int, max int) int {
 	seed, _ := crand.Int(crand.Reader, big.NewInt(math.MaxInt64))
 	rand.Seed(seed.Int64())
 	return rand.Intn((max+1)-min) + min
+}
+
+// for dev
+func cors() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
+		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
+		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
+		c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT, DELETE")
+
+		if c.Request.Method == "OPTIONS" {
+			c.AbortWithStatus(204)
+			return
+		}
+
+		c.Next()
+	}
 }
